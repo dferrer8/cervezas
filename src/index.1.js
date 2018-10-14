facd3a203ca70cc816b4826617dac03a610280dc
dferrer8 <david.dfg@gmail.com>
2018-10-14 13:22:03 +0200

    Modificación para usar la libreria lodash

--- src/index.1.js
+++ src/index.1.js
@@ -1,9 +1,7 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array') // Añadimos la libreria necesaria para sacar cerveza aleatoria
-const _ = require('lodash')
 
 module.exports = {
   todas: cervezas,
-  todasOrdenadas: _.sortBy(cervezas, [nombre]),
   alzar: uniqueRandomArray(cervezas)
 }
